8afb76a6cfb7a0c8d8571e73430f35f5def6da38
Owen Dismuke <owen.dismuke@gmail.com>
2015-08-24 14:59:40 -0700

    Track successful user login

--- client/landingPage/landingPage.js
+++ client/landingPage/landingPage.js
@@ -1,12 +1,14 @@
 angular.module('app.landingPage',[])
 
-.controller('landingPageController', function ($scope, $http, auth, store, $state) {
-  $scope.login = function () {
-    auth.signin({}, function (profile, token) {
-      // Success callback
-      store.set('profile', profile);
-      store.set('token', token);
-      $http.post('/api/getProfile', {id: auth.profile.user_id.split('|')[1]})
+  .controller('landingPageController', ['$scope', '$http', 'auth', 'store', '$state', '$analytics',
+    function ($scope, $http, auth, store, $location, $analytics) {
+      $scope.login = function () {
+        auth.signin({}, function (profile, token) {
+          // Success callback
+          $analytics.eventTrack('Login', {category: 'User', label: profile.name});
+          store.set('profile', profile);
+          store.set('token', token);
+          $http.post('/api/getProfile', {id: auth.profile.user_id.split('|')[1]})
         .then(function(res){
           if(!!res.data){
             $state.go('performance');
@@ -18,5 +20,5 @@ angular.module('app.landingPage',[])
       // Error callback
     });
   };
-});
+}]);
 
